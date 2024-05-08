61acd9aa16d4804bb101ca7d12c94954a161c2e6
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-05-08 14:07:08 +0530

    Create doWhile.cpp

---
+++ LoopingStatement/doWhile.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the limit : ";
+    cin>>n;
+    int i = 1;
+    do{
+        cout<<"\n"<<i;
+        i++;
+    }while(i<n);
+    return 0;
+}
