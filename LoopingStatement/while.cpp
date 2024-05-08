be1d0c49a90d35b26a35b83fba24c9e047673f43
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-05-08 09:20:54 +0530

    Create while.cpp

---
+++ LoopingStatement/while.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the limit : ";
+    cin>>n;
+    int i = 1;
+    while(i<n){
+        cout<<"\n"<<i;
+        i++;
+    }
+    return 0;
+}
