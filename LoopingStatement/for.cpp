905f6c5db2180403cfca5c8b16a8ba9e71bf5ba8
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-05-08 14:19:43 +0530

    Create for.cpp

---
+++ LoopingStatement/for.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the limit : ";
+    cin>>n;
+    for(int i=1;i<=10;i++){
+        cout<<"\n"<<i<<" * "<<n<<" = "<<i*n;
+    }
+    return 0;
+}
